8543f90aed8020e2bf1e53475bf2850f099262fa
Mateus Araújo <mateusaraujo996@gmail.com>
2020-08-21 23:31:02 -0300

    Criação do projeto da Pokedex com Spring Webflux

---
+++ src/test/java/com/webflux/pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
